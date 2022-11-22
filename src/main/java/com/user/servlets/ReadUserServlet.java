69b7dc0b73aabe342ceb95fe3f9b8696a9d78e9b
Elise Melo <elisemelo@Elises-MacBook-Air.local>
2022-11-22 17:00:38 -0300

    Creating static html

---
+++ src/main/java/com/user/servlets/ReadUserServlet.java
@@ -0,0 +1,73 @@
+package com.user.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Servlet implementation class CreateUserServlet
+ */
+@WebServlet("/readServlet")
+public class ReadUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+	private Statement statement;
+      
+        public void init() {
+        	try {
+        		connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "test");
+        	} catch (SQLException e) {
+        		e.printStackTrace();
+        	}
+        	
+        	
+        }
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("select * from user");
+			PrintWriter out = response.getWriter();
+			out.print("<table>");
+			out.print("<tr>");
+			out.print("<th>");
+			out.print("<th>");
+			out.print("First Name");
+			out.print("</th>");
+			out.print("<th>");
+			out.print("Last Name");
+			out.print("</th>");
+			out.print("<th>");
+			out.print("Email");
+			out.print("</th>");
+			out.print("</tr>");
+			out.print("</table>");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void destroy() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
